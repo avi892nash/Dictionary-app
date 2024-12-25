c894afcd872f087132db299a8133e3f97fedd536
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-26 01:33:20 +0530

    DA-2 Completed home page UI

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import { Box, Typography, FilledInput, IconButton } from "@mui/material";
-import { Search as SearchIcon, Bookmark as BookmarkIcon } from "@mui/icons-material";
+import { Search as SearchIcon, Bookmark as BookmarkIcon, Gradient } from "@mui/icons-material";
 
 const Home = ()=>{
     return(
@@ -15,14 +15,44 @@ const Home = ()=>{
                 }
         >
             <img src="/assets/book.png" alt="book-img"/>
-            <Typography variant="h4">
+            <Typography 
+            color="primary"
+            sx={{
+                mt: 3,
+                mb:1
+            }}
+            
+            variant="h4">
                 Dictionary
             </Typography>
-            <Typography>
+            <Typography color="GrayText">
                find meanings and save for quick refrence
             </Typography>
-            <FilledInput />
-            <IconButton>
+            <Box
+                sx={{width: '360px'}}
+            >
+            <FilledInput disableUnderline placeholder="Search word"
+                sx={{
+                    my: 4,
+                    backgroundColor: 'white',
+                    borderRadius: 2,
+                    boxShadow: '0px 10px 25px rgba(0,0,0,0.05)',
+                    '& .MuiFilledInput-input':{
+                        p:2
+                    }
+                }}
+                startAdornment={ <SearchIcon color="disabled" />}
+                fullWidth
+            /></Box>
+            <IconButton 
+                sx={{
+                    borderRadius: 2,
+                    p: 2,
+                   background : 'linear-gradient(138.72deg, #DC8295 0%, #DC687C 95.83%)',
+                   boxShadow: '0px 10px 10px rgba(221, 114, 133, 0.2)',
+                   color: '#fff'
+                }}
+            >
                 <BookmarkIcon />
             </IconButton>
         </Box>
--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,15 @@ export default createTheme({
     palette:{
         background:{
             default: '#F1F3F4'
+        },
+        primary:{
+            main: "#14194c"
         }
+    },
+    typography:{
+        h4: {
+            fontWeight: 800
+        },
+        fontFamily:'Mulish'
     }
 })
